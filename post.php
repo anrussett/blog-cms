84186a082c9587f32c1bc59838269c198f1426f1
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-09-26 18:52:18 -0400

    added php to post

--- post.php
+++ post.php
@@ -26,17 +26,6 @@ include 'header.php';
     <div> <?php echo $postDetails["author"]; ?> </div>
     <div> <?php echo $postDetails["date"]; ?> </div>
     <div> <?php echo $postDetails["content"]; ?> </div>
-    
-            <h2>Post One</h2>
-                <div>
-                    <h4>Date:9/26/2020</h4>
-                </div>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro possimus pariatur quos nam aut est, incidunt delectus in? At, ipsum delectus facere eum ducimus temporibus. Minima non asperiores eveniet rem?</p>
-                </div>
-                <div>
-                    <h4>Author: Andrew Russett</h4>
-                </div>
         </main>
     </html>
 
