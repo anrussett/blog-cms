3f1fe17e3211cf33f2277ef0ee3239a51a5fe4e1
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-09-26 17:22:36 -0400

    Updated tab layout on header.php

--- header.php
+++ header.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-    <html>
-<div id="header">
-<header>
-    <h1>Andrew's Blog CMS</h1>
-    <link rel="stylesheet" type="text/css" href="format.css" />
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="post-submission.php">Blog Post</a>
-    </nav>
-</header>
-</div>
+<html>
+    <div id="header">
+        <header>
+            <h1>Andrew's Blog CMS</h1>
+            
+            <link rel="stylesheet" type="text/css" href="format.css" />
+           
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="post-submission.php">Blog Post</a>
+            </nav>
+        </header>
+    </div>
+</html>
