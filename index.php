b53518653557d9efb8d8948ccb9c29ebdbd4bbde
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-09-26 18:28:40 -0400

    updated errors

--- index.php
+++ index.php
@@ -3,26 +3,28 @@ include 'header.php';
 ?>
 
 <!DOCTYPE html>
-    <html>
+<html>
+    <main>
     <?php
         function getPostTitlesFromDatabase() {
             //TODO in Module 4
-            // get this data from a database instead of hardcoding it
+            //get this data from a database instead of hardcoding it
             $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
             return $postTitles;
         }
     ?>
-        <main>
             <ul>
                 <?php
                     $postTitles = getPostTitlesFromDatabase();
 
                     foreach($postTitles as $postTitle) {
-                        echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
+                        echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . 
+                        "</a></li>";
+                    }
                 ?>
             </ul>
-        </main>
-    <html>
+    </main>
+</html>
 
 <?php
 include 'footer.php';
