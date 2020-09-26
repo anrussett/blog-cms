9b998b270b5fefb0cbe162cead4f59767004a52b
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-09-26 17:07:35 -0400

    include header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+    <h1>Andrew's Blog CMS</h1>
+    <link rel="stylesheet" type="text/css" href="format.css" />
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="post-submission.php">Blog Post</a>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
     <html>
         <div id="header">
-            <header>
-                <h1>Andrew's Blog CMS</h1>
-                <link rel="stylesheet" type="text/css" href="format.css" />
-                <nav>
-                    <a href="index.php">Home</a>
-                    <a href="post-submission.php">Blog Post</a>
-                </nav>
-            </header>
+            include 'header.php';
         </div><!--close for header--> 
 
         <main>
