c19bebe882e691e443530e33d5e21614abffdd77
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-02 11:55:05 -0400

    trying to connect to data

--- post.php
+++ post.php
@@ -10,7 +10,7 @@ include_once 'includes/db_connect.php';
     <html>
         <main>
 
-            <?php
+        <?php
 
             function getPostDetailsFromDatabase() {
                 // Get the post title
@@ -18,16 +18,15 @@ include_once 'includes/db_connect.php';
 
 
                 // Get the post that matches the postTitle
-                include_once 'includes/db_connect.php';
+                include_once'includes/db_connect.php';
                 $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
                 $result = mysqli_query($conn, $sql);
-
 
                 // Get the first row from the result as an associative array
                 $postDetails = mysqli_fetch_assoc($result);
                 return $postDetails;
             }
-            ?>
+        ?>
 
         </main>
     </html>
