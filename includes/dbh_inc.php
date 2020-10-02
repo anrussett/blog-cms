1aaa29ede779e1bf9038ab5cea9fa92b03795da5
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-02 09:38:46 -0400

    Updated connection to database

--- includes/dbh_inc.php
+++ includes/dbh_inc.php
@@ -1,3 +1,10 @@
 <?php
 
-$dbServername = ""
+$dbServername = "localhost";
+$dbUsername = "anrussett"; //This is the same username as phpMyAdmin
+$dbPassword = "AdvancedWebProjects"; //This is the same password as phpMyAdmin
+$dbName = "blog_cms";
+
+//create connection
+$conn=mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
+?>
