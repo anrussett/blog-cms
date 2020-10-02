30b5a1a7abb4e132f37a6b285f4de5101a6555da
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-02 10:49:06 -0400

    updated include to show accurate title

--- dbh_inc.php
+++ dbh_inc.php
@@ -1,8 +1,4 @@
 <?php
-include_once'db_connect.php';
-?>
-
-<?php
 $dbServername = "localhost";
 $dbUsername = "anrussett"; //This is the same username as phpMyAdmin
 $dbPassword = "AdvancedWebProjects"; //This is the same password as phpMyAdmin
