8bb13efb08999509160e569b2d9f1eba295ae379
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-02 10:18:11 -0400

    deleated include code

--- includes/dbh_inc.php
+++ includes/dbh_inc.php
@@ -1,5 +1,4 @@
 <?php
-include_once'db_connect.php';
 $dbServername = "localhost";
 $dbUsername = "anrussett"; //This is the same username as phpMyAdmin
 $dbPassword = "AdvancedWebProjects"; //This is the same password as phpMyAdmin
