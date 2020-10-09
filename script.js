85c7b2aa852291c125bb0461549e63ae7f44f44d
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-09 11:01:52 -0400

    adding list.js

--- script.js
+++ script.js
@@ -4,28 +4,3 @@ $(document).ready(function() {
     })
 });
 
-
-var options = {
-    valueNames: [ 'title' ]
-};
-new List('sortable-blog-list', options);
-
-var editor = ace.edit("editor");
-editor.session.setMode("ace/mode/html");
-
-$("#submit").click(function(){
-    var title = $("#title").val();
-    var author = $("#author").val();
-    var date = $("#date").val();
-    var content = editor.getValue();
-});
-
-
-$.post( "submit-post.php", { 
-    title: title, 
-    content: content, 
-    author: author, 
-    date: date 
-}).done(function() {
-window.location = "/index.php";
-});
