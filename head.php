d634feb0c1b800deb376290f2ff2db1c59fc6d34
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-09 13:07:53 -0400

    add ace.js

--- head.php
+++ head.php
@@ -5,6 +5,7 @@
         
             <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
             <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js" integrity="sha512-GZ1RIgZaSc8rnco/8CXfRdCpDxRCphenIiZ2ztLy3XQfCbQUSCuk8IudvNHxkRA3oUg6q0qejgN/qqyG1duv5Q==" crossorigin="anonymous"></script>
             <script src="script.js"></script>
             
     
--- post-submission.php
+++ post-submission.php
@@ -15,12 +15,33 @@ include'header.php';
         ?>
     </head>
     <main>
+    <link rel="stylesheet" type="text/css" href="format.css" />
         <form action="submit-post.php" method="POST">
             <input type="text" id="title" name="title" placeholder="Title"></input>
             <input type="text" id="author" name="author" placeholder="Author"></input>
             <input type="date" id="date" name="date" placeholder="Date"></input>
             <div id="editor"></div>
+            <script>
+            var editor=ace.edit("editor");
+            editor.session.setMode("ace/mode/html");
+            </script>
             <button id="submit">Submit</input>  
+            <script>
+            $("#submit").click(function() {
+                var title = $("#title").val();
+                var author = $("#author").val();
+                var date = $("#date").val();
+                var content = editor.getValue();
+            });
+            $.post( "submit-post.php", { 
+                    title: title, 
+                    content: content, 
+                    author: author, 
+                    date: date 
+            }).done(function() {
+                window.location = "/index.php";
+            });
+            </script>
         </form>
     </main>
 </html>
