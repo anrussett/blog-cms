976309f6398d73f60566a2b2eeac315071e8e947
Andrew Russett <andrew@Andrews-MacBook-Pro.local>
2020-10-09 11:26:56 -0400

    added list.js

--- index.php
+++ index.php
@@ -12,6 +12,24 @@
     <body>
         <br>
         <button id="hello">Hello</button>
+        <div id="sortable-blog-list">
+            <input class="search" placeholder="Search"/>
+            <button class="sort" data-sort="title">Sort</button>
+            <ul class="list">
+            <?php
+                $postTitles = getPostTitlesFromDatabase();
+                foreach($postTitles as $postTitle) {
+                echo "<li><a href='post.php?title=" . $postTitle . "' class='title'>" . $postTitle . "</a></li>";
+                    }
+            ?>
+            <script>
+            var options = {
+                valueNames: ['title']
+            };
+            new List('sortable-blog-list', options);
+            </script>
+            </ul>
+        </div>
         <?php
 
             function getPostTitlesFromDatabase() {
@@ -31,19 +49,6 @@
                 return $postTitles;
             }
         ?>
-
-        <div id="sortable-blog-list">
-            <input class="search" placeholder="Search" />
-            <button class="sort" data-sort="title">Sort</button>
-            <ul class="list">
-            <?php
-                $postTitles = getPostTitlesFromDatabase();
-                foreach($postTitles as $postTitle) {
-                echo "<li><a href='post.php?title=" . $postTitle . "' class='title'>" . $postTitle . "</a></li>";
-                    }
-            ?>
-            </ul>
-        </div>
 
     </body>
 
